6fc712266c83769e6e42b5701312b8e7375ccefd
zeroclock <zeroclock@icloud.com>
2020-12-27 22:53:25 +0900

    Add tests for event area

--- src/ui/event_area.rs
+++ src/ui/event_area.rs
@@ -36,6 +36,18 @@ where
     }
 }
 
+impl<B> Default for EventArea<B>
+where
+    B: Backend,
+{
+    fn default() -> Self {
+        EventArea {
+            is_selected: false,
+            _phantom: PhantomData,
+        }
+    }
+}
+
 #[async_trait]
 impl<B> Drawable<B> for EventArea<B>
 where
@@ -56,5 +68,71 @@ where
 
     async fn handle_event(&mut self, _event: KeyEvent) -> bool {
         false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crossterm::event::{KeyCode, KeyModifiers};
+    use tui::{backend::TestBackend, buffer::Buffer, style::Color};
+
+    use super::*;
+    use crate::test_helper::get_test_terminal;
+
+    fn test_case(event_area: &mut EventArea<TestBackend>, color: Color, lines: Vec<&str>) {
+        let mut terminal = get_test_terminal(20, 10);
+        let lines = if lines.len() > 0 {
+            lines
+        } else {
+            vec![
+                "┌Events────────────┐",
+                "│                  │",
+                "│                  │",
+                "│                  │",
+                "│                  │",
+                "│                  │",
+                "│                  │",
+                "│                  │",
+                "│                  │",
+                "└──────────────────┘",
+            ]
+        };
+        let mut expected = Buffer::with_lines(lines);
+        for y in 0..10 {
+            for x in 0..20 {
+                let ch = expected.get_mut(x, y);
+                if y == 0 || y == 9 {
+                    ch.set_fg(color);
+                } else {
+                    if ch.symbol == "│" {
+                        ch.set_fg(color);
+                    }
+                }
+            }
+        }
+        terminal
+            .draw(|f| {
+                event_area.draw(f, f.size());
+            })
+            .unwrap();
+        terminal.backend().assert_buffer(&expected);
+    }
+
+    #[tokio::test]
+    async fn test_draw() {
+        let mut event_area: EventArea<TestBackend> = EventArea::default();
+        test_case(&mut event_area, Color::White, vec![]);
+        event_area.set_select(true);
+        test_case(&mut event_area, Color::Yellow, vec![]);
+    }
+
+    #[tokio::test]
+    async fn test_handle_event() {
+        let mut event_area: EventArea<TestBackend> = EventArea::default();
+        assert!(
+            !event_area
+                .handle_event(KeyEvent::new(KeyCode::Up, KeyModifiers::NONE))
+                .await
+        );
     }
 }

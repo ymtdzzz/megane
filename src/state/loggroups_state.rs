8440ac7ef0176eb82a72dccf972abd901fa6082c
zeroclock <zeroclock@icloud.com>
2020-12-28 17:48:17 +0900

    Add tests for loggroup state

--- src/state/loggroups_state.rs
+++ src/state/loggroups_state.rs
@@ -61,12 +61,64 @@ impl LogGroupsState {
 
 #[cfg(test)]
 mod tests {
+    use tui::widgets::{ListItem, ListState};
+
     use super::*;
+    use crate::test_helper::make_log_groups;
 
     #[test]
     fn test_new() {
         let mut state = LogGroupsState::new();
         state.is_fetching = true;
         assert!(state.is_fetching);
+    }
+
+    #[test]
+    fn test_get_list_items() {
+        let mut state = LogGroupsState::new();
+        state.log_groups = LogGroups::new(make_log_groups(0, 3));
+        state.selection = vec![0, 2];
+        let exp_item = vec![
+            ListItem::new("[X]log_group_0"),
+            ListItem::new("[ ]log_group_1"),
+            ListItem::new("[X]log_group_2"),
+            ListItem::new("[ ]log_group_3"),
+        ];
+        let exp_state = ListState::default();
+        let (res_item, res_state) = state.get_list_items();
+        assert_eq!(exp_item, res_item);
+        assert_eq!(exp_state.selected(), res_state.selected());
+    }
+
+    #[test]
+    fn test_select() {
+        let mut state = LogGroupsState::new();
+        state.selection = vec![0, 2];
+        state.select(0);
+        state.select(0);
+        state.select(1);
+        state.select(2);
+        state.select(3);
+        state.select(4);
+        // don't add idx 5 because MAX_LOG_SELECTION is 4
+        state.select(5);
+        let expect = vec![0, 1, 3, 4];
+        assert_eq!(expect, state.selection);
+    }
+
+    #[test]
+    fn test_get_selected_log_group_names() {
+        let mut state = LogGroupsState::new();
+        state.log_groups = LogGroups::new(make_log_groups(0, 5));
+        state.selection = vec![0, 2, 4];
+        let result = state.get_selected_log_group_names();
+        let mut expect = make_log_groups(0, 4);
+        expect.remove(3);
+        expect.remove(1);
+        let expect = expect
+            .iter_mut()
+            .map(|v| v.log_group_name.as_ref().unwrap().to_string())
+            .collect::<Vec<String>>();
+        assert_eq!(expect, result);
     }
 }

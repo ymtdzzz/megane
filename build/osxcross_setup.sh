84c2bf80635ca4a252eb5bf23c5f66fc5c206b5a
zeroclock <zeroclock@icloud.com>
2021-02-03 18:25:20 +0900

    [CI] fix osxcross_setup

--- build/osxcross_setup.sh
+++ build/osxcross_setup.sh
@@ -3,4 +3,4 @@ cd osxcross
 wget -nc https://s3.dockerproject.org/darwin/v2/MacOSX10.10.sdk.tar.xz
 mv MacOSX10.10.sdk.tar.xz tarballs/
 UNATTENDED=yes OSX_VERSION_MIN=10.7 ./build.sh
-PATH="$(pwd)/osxcross/target/bin:$PATH"
+export PATH="$(pwd)/osxcross/target/bin:$PATH"

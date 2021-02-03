b09e89413dd83b00f18b1f8e8b500f0ad6e03a73
zeroclock <zeroclock@icloud.com>
2021-02-03 19:49:54 +0900

    [CI] export PATH at the script phase

--- build/osxcross_setup.sh
+++ build/osxcross_setup.sh
@@ -3,4 +3,3 @@ cd osxcross
 wget -nc https://s3.dockerproject.org/darwin/v2/MacOSX10.10.sdk.tar.xz
 mv MacOSX10.10.sdk.tar.xz tarballs/
 UNATTENDED=yes OSX_VERSION_MIN=10.7 ./build.sh
-export PATH="$(pwd)/osxcross/target/bin:$PATH"
